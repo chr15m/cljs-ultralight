b0fc46380cdab7e6483b77e4fd2b062e75da62d7
Chris McCormick <chris@mccormick.cx>
2022-12-22 09:36:12 +0800

    Added log and single-selector.

--- src/ultralight/core.cljs
+++ src/ultralight/core.cljs
@@ -1,10 +1,14 @@
 (ns ultralight.core)
 
+(defn log [& args] (apply js/console.log (clj->js args)) (first args))
+
 (defn js-map [a f] (.map (js/Array.from a) f))
 (defn js-filter [a f] (.filter (js/Array.from a) f))
 
 (defn $ [q] (.querySelectorAll js/document q))
+(defn $1 [q] (.querySelector js/document q))
 (defn text [elements txt] (js-map elements (fn [el] (aset el "textContent" txt) el)))
 (defn attr [elements k v] (js-map elements (fn [el] (.setAttribute el k v) el)))
 (defn del [elements] (js-map elements (fn [el] (js/console.log "el" el) (.remove el) el)))
 (defn evt [elements event callback] (js-map elements (fn [el] (.addEventListener el event callback))))
+
