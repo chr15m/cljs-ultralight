507f0e1056c6035b5e9d453fddc82232df38a993
Chris McCormick <chris@mccormick.cx>
2022-12-22 10:23:06 +0800

    Removed stray log statement.

--- src/ultralight/core.cljs
+++ src/ultralight/core.cljs
@@ -9,6 +9,6 @@
 (defn $1 [q] (.querySelector js/document q))
 (defn text [elements txt] (js-map elements (fn [el] (aset el "textContent" txt) el)))
 (defn attr [elements k v] (js-map elements (fn [el] (.setAttribute el k v) el)))
-(defn del [elements] (js-map elements (fn [el] (js/console.log "el" el) (.remove el) el)))
+(defn del [elements] (js-map elements (fn [el] (.remove el) el)))
 (defn evt [elements event callback] (js-map elements (fn [el] (.addEventListener el event callback))))
 
