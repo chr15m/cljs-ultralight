380328dc51b763c12b7be912060f9a708142236c
Chris McCormick <chris@mccormick.cx>
2023-02-21 20:34:14 +0800

    Make el optional without bloating size.

--- src/ultralight/core.cljs
+++ src/ultralight/core.cljs
@@ -3,8 +3,8 @@
 (defn js-map [a f] (.map (js/Array.from a) f))
 (defn js-filter [a f] (.filter (js/Array.from a) f))
 
-(defn $ [q] (.querySelector (or (aget (js* "arguments") 2) js/document) q))
-(defn $$ [q] (.querySelectorAll (or (aget (js* "arguments") 2) js/document) q))
+(defn $ ([q el] (.querySelector (or el js/document) q)) ([q] ($ q nil)))
+(defn $$ ([q el] (.querySelectorAll (or el js/document) q)) ([q] ($$ q nil)))
 (defn text [elements txt] (js-map elements (fn [el] (aset el "textContent" txt) el)))
 (defn attr [elements k v] (js-map elements (fn [el] (.setAttribute el k v) el)))
 (defn del [elements] (js-map elements (fn [el] (.remove el) el)))
